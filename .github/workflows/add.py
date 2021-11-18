0f11d849604f37f325b27cd47017b4e212f289c7
CyberHeck <94623843+CyberHeck@users.noreply.github.com>
2021-11-19 00:06:08 +0700

    Update add.py

--- .github/workflows/add.py
+++ .github/workflows/add.py
@@ -1,4 +1,10 @@
-import os
-print ("Deleting Proxy List Please Wait!")
-os.system("rm -r proxy.txt")
-print ("Done!")
+import requests
+  
+link = input("Enter your link: ")
+url = link
+r = requests.get(url, allow_redirects=True)
+
+open('proxy.txt', 'wb').write(r.content)
+print("Now Your Proxy list is Installing Done!")
+print("File name is Proxy.txt")
+print("If You Install wrong list or wont working run srcipt delete!")
