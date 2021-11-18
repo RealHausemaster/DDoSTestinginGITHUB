d4b135dc9131737dcac85fd99257d6bbd41a5352
CyberHeck <94623843+CyberHeck@users.noreply.github.com>
2021-11-19 00:10:12 +0700

    Add files via upload

---
+++ config/setting.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install xfce4 xfce4-terminal
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg -i chrome*
+sudo apt-get install -f
